6ab223fbcd22fd4a50d3d65ffd9db1c727f53674
Ilya-Chayka99 <Ilya-Chayka99@yandex.ru>
2022-06-29 20:41:22 +0400

    2й уровень , переделка персонажа

--- app.js
+++ app.js
@@ -11,19 +11,23 @@ window.addEventListener('load',function (){
         constructor(width,height) {
             this.width= width
             this.heigth=height
-            this.groundMargin=80
+            this.groundMargin=60
             this.speed=0
             this.maxSpeed=2
             this.background=new Background(this)
             this.player=new Player(this)
             this.input=new InputHandler(this)
             this.UI=new UI(this)
+            this.collisions=[]
             this.enemies=[]
+            this.particles=[]
             this.enemyTimer=0
             this.enemyUnterval=1000
-            this.debug=true
+            this.debug=false
             this.score=0
             this.fontColor='black'
+            this.player.currentState=this.player.states[0]
+            this.player.currentState.enter()
         }
         update(delTime)
         {
@@ -38,6 +42,15 @@ window.addEventListener('load',function (){
                 enemy.update(delTime)
                 if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy),1)
             })
+            this.particles.forEach((particle,i)=>{
+                if(particle.matkedForDeletion) this.particles.splice(i,1)
+                particle.update()
+            })
+            if(this.particles.length>500) this.particles=this.particles.splice(0,500)
+            this.collisions.forEach((col,i)=>{
+                col.update(delTime)
+                if(col.matkedForDeletion) this.collisions.splice(i,1)
+            })
 
         }
         draw(context)
@@ -46,6 +59,12 @@ window.addEventListener('load',function (){
             this.player.draw(context)
             this.enemies.forEach(enemy=>{
                 enemy.draw(context)
+            })
+            this.particles.forEach(particle=>{
+                particle.draw(context)
+            })
+            this.collisions.forEach(coll=>{
+                coll.draw(context)
             })
             this.UI.draw(context)
         }
@@ -62,8 +81,10 @@ window.addEventListener('load',function (){
         constructor(game)
         {
             this.game=game
-            this.width=100
-            this.height=91.3
+            this.width=55.56
+            this.height=100
+            // this.width=100
+            // this.height=91.3
             this.x=0
             this.y=this.game.heigth - this.height - this.game.groundMargin
             this.image = document.querySelector('#player')
@@ -71,9 +92,8 @@ window.addEventListener('load',function (){
             this.maxSpeed=5
             this.vy=0
             this.wait=0.5
-            this.states=[new Sitting(this),new Running(this),new Jumping(this),new Falling(this),new Rolling(this)]
-            this.currentState=this.states[0]
-            this.currentState.enter()
+            this.states=[new Sitting(this.game),new Running(this.game),new Jumping(this.game),new Falling(this.game),
+                new Rolling(this.game),new Diving(this.game),new Hit(this.game)]
             this.frameX =0
             this.frameY =0
             this.maxFrame = 5
@@ -102,6 +122,7 @@ window.addEventListener('load',function (){
             this.y+=this.vy
             if(!this.onGround()) this.vy+=this.wait
             else this.vy=0
+            if(this.y>this.game.heigth-this.height-this.game.groundMargin) this.y=this.game.heigth-this.height-this.game.groundMargin
             //анимация
             if(this.frameTimer>this.frameInterval)
             {
@@ -137,7 +158,10 @@ window.addEventListener('load',function (){
                 if(enemy.x<this.x+this.width && enemy.x+enemy.width>this.x && enemy.y<this.y+this.height && enemy.y+enemy.height>this.y)
                 {
                     enemy.markedForDeletion=true
-                    this.game.score++
+                    this.game.collisions.push(new CollisionAnimation(this.game,enemy.x+enemy.width*0.5,enemy.y+enemy.height*0.5))
+                    if (this.currentState===this.states[4]||this.currentState===this.states[5])
+                        this.game.score++
+                    else this.setState(6,0)
                 }
 
             })
@@ -150,14 +174,14 @@ window.addEventListener('load',function (){
             this.game=game
             this.keys=[]
             window.addEventListener('keydown',e=>{
-                if((e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight' || e.key === 'Control' || e.key==='1') && this.keys.indexOf(e.key) === -1)
+                if((e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'ArrowLeft' || e.key === 'ArrowRight' || e.key === 'Control' || e.key==='w') && this.keys.indexOf(e.key) === -1)
                 {
                     this.keys.push(e.key)
                 }else if(e.key==='d') this.game.debug=!this.game.debug
             })
 
             window.addEventListener('keyup',e=>{
-                if(e.key === 'ArrowDown' || e.key === 'ArrowUp'  || e.key === 'ArrowLeft' || e.key === 'ArrowRight' || e.key === 'Control' || e.key === '1')
+                if(e.key === 'ArrowDown' || e.key === 'ArrowUp'  || e.key === 'ArrowLeft' || e.key === 'ArrowRight' || e.key === 'Control' || e.key === 'w')
                 {
                     this.keys.splice(this.keys.indexOf(e.key),1)
                 }
@@ -177,107 +201,154 @@ window.addEventListener('load',function (){
         HIT:6,
     }
     class State {
-        constructor(state)
+        constructor(state,game)
         {
             this.state = state
+            this.game=game
         }
     }
 //состояния персонажа сидеть
     class Sitting extends State{
-        constructor(player) {
-            super('SITTING');
-            this.player = player
+        constructor(game) {
+            super('SITTING',game);
         }
         enter(){
-            this.player.frameX=0
-            this.player.maxFrame=4
-            this.player.frameY=5
+            this.game.player.frameX=0
+            this.game.player.maxFrame=11
+            this.game.player.frameY=0
         }
         handleInput(input){
             if(input.includes('ArrowLeft') || input.includes('ArrowRight'))
             {
-                this.player.setState(states.RUNNING,1)
-            } else if(input.includes('1')) this.player.setState(states.ROLLING,2)
+                this.game.player.setState(states.RUNNING,1)
+            } else if(input.includes('w')) this.game.player.setState(states.ROLLING,2)
         }
     }
     //состояния персонажа бежать
     class Running extends State{
-        constructor(player) {
-            super('RUNNING');
-            this.player = player
+        constructor(game) {
+            super('RUNNING',game);
         }
         enter(){
-            this.player.frameX=0
-            this.player.maxFrame=6
-            this.player.frameY=3
+            this.game.player.frameX=0
+            this.game.player.maxFrame=11
+            this.game.player.frameY=0
         }
         handleInput(input){
+            this.game.particles.push(new Dust(this.game,this.game.player.x+this.game.player.width*0.5,this.game.player.y+this.game.player.height))
             if(input.includes('ArrowDown'))
             {
-                this.player.setState(states.SITTING,0)
+                this.game.player.setState(states.SITTING,0)
             } else if(input.includes('ArrowUp'))
             {
-                this.player.setState(states.JUMPING,1)
-            } else if(input.includes('1')) this.player.setState(states.ROLLING,2)
+                this.game.player.setState(states.JUMPING,1)
+            } else if(input.includes('w')) this.game.player.setState(states.ROLLING,2)
         }
     }
     //состояния персонажа прыжок
     class Jumping extends State{
-        constructor(player) {
-            super('JUMPING');
-            this.player = player
+        constructor(game) {
+            super('JUMPING',game);
         }
         enter(){
-            if(this.player.onGround()) this.player.vy-=20
-            this.player.frameX=0
-            this.player.maxFrame=6
-            this.player.frameY=1
+            if(this.game.player.onGround()) this.game.player.vy-=20
+            this.game.player.frameX=0
+            this.game.player.maxFrame=7
+            this.game.player.frameY=2
         }
         handleInput(input){
-            if(this.player.vy > this.player.wait)
+            if(this.game.player.vy > this.game.player.wait)
             {
-                this.player.setState(states.FALLING,1)
-            } else if(input.includes('1')) this.player.setState(states.ROLLING,2)
+                this.game.player.setState(states.FALLING,1)
+            } else if(input.includes('w')) this.game.player.setState(states.ROLLING,2)
+            else if(input.includes('ArrowDown')) this.game.player.setState(states.DIVING,0)
         }
     }
     //состояния персонажа падение
     class Falling extends State{
-        constructor(player) {
-            super('FALLING');
-            this.player = player
+        constructor(game) {
+            super('FALLING',game);
         }
         enter(){
-            this.player.frameX=0
-            this.player.maxFrame=6
-            this.player.frameY=2
+            this.game.player.frameX=0
+            this.game.player.maxFrame=4
+            this.game.player.frameY=3
         }
         handleInput(input){
-            if(this.player.onGround())
+            if(this.game.player.onGround())
             {
-                this.player.setState(states.RUNNING,1)
-            }
+                this.game.player.setState(states.RUNNING,1)
+            } else if(input.includes('ArrowDown')) this.game.player.setState(states.DIVING,0)
         }
     }
     class Rolling extends State{
-        constructor(player) {
-            super('ROLLING');
-            this.player = player
+        constructor(game) {
+            super('ROLLING',game);
         }
         enter(){
-            this.player.frameX=0
-            this.player.maxFrame=6
-            this.player.frameY=6
+            this.game.player.frameX=0
+            this.game.player.maxFrame=10
+            this.game.player.frameY=4
         }
         handleInput(input){
-            if(!input.includes('1') && this.player.onGround())
+            this.game.particles.push(new Fire(this.game,this.game.player.x+this.game.player.width*0.6,this.game.player.y+this.game.player.height*0.25))
+
+            if(!input.includes('w') && this.game.player.onGround())
             {
-                this.player.setState(states.RUNNING,1)
-            } else if(!input.includes('1') && !this.player.onGround())
+                this.game.player.setState(states.RUNNING,1)
+            } else if(!input.includes('w') && !this.game.player.onGround())
             {
-                this.player.setState(states.FALLING, 1)
-            }else if(input.includes('1') && input.includes('ArrowUp') && this.player.onGround())
+                this.game.player.setState(states.FALLING, 1)
+            }else if(input.includes('w') && input.includes('ArrowUp') && this.game.player.onGround())
             {
-                this.player.vy-=20
+                this.game.player.vy-=20
+            }  else if(input.includes('ArrowDown')&&!this.game.player.onGround()) this.game.player.setState(states.DIVING,0)
+
+        }
+    }
+
+    class Diving extends State{
+        constructor(game) {
+            super('DIVING',game);
+        }
+        enter(){
+            this.game.player.frameX=0
+            this.game.player.maxFrame=6
+            this.game.player.frameY=6
+            this.game.player.vy=15
+        }
+        handleInput(input){
+            this.game.particles.push(new Fire(this.game,this.game.player.x+this.game.player.width*0.5,this.game.player.y+this.game.player.height*0.5))
+
+            if(this.game.player.onGround())
+            {
+                this.game.player.setState(states.RUNNING,1)
+                for(let i=0;i<30;i++)
+                this.game.particles.push(new Splash(this.game,this.game.player.x+this.game.player.width*0.5,this.game.player.y+this.game.player.height))
+            } else if(input.includes('w') && this.game.player.onGround())
+            {
+                this.game.player.setState(states.ROLLING, 2)
+            }
+
+        }
+    }
+
+    class Hit extends State{
+        constructor(game) {
+            super('HIT',game);
+        }
+        enter(){
+            this.game.player.frameX=0
+            this.game.player.maxFrame=11
+            this.game.player.frameY=1
+        }
+        handleInput(input){
+            if(this.game.player.frameX>=10 && this.game.player.onGround())
+            {
+                this.game.player.setState(states.RUNNING,1)
+            } else if(this.game.player.frameX>=10 && !this.game.player.onGround())
+            {
+                this.game.player.setState(states.FALLING, 1)
             }
 
         }
@@ -290,14 +361,113 @@ window.addEventListener('load',function (){
         }
         update()
         {
-            this.x-=this.speedX+this.speed
+            this.x-=this.speedX+this.game.speed
             this.y-=this.speedY
             this.size*=0.95
             if(this.size<0.5) this.matkedForDeletion=true
         }
     }
+    class Dust extends Particle{
+        constructor(game,x,y) {
+            super(game);
+            this.size=Math.random()*10+10
+            this.x=x
+            this.y=y
+            this.speedX=Math.random()
+            this.speedY=Math.random()
+            this.color='rgba(0,0,0,0.2)'
 
+        }
+        draw(context)
+        {
+            context.beginPath()
+            context.arc(this.x,this.y,this.size,0,Math.PI*2)
+            context.fillStyle=this.color
+            context.fill()
+        }
+    }
+    class Splash extends Particle{
+        constructor(game,x,y) {
+            super(game);
+            this.size=Math.random()*100+100
+            this.x=x-this.size*0.4
+            this.y=y-this.size*0.5
+            this.speedX=Math.random()*6-4
+            this.speedY=Math.random()*2+2
+            this.gravity=0
+            this.image=document.querySelector('#fire')
+        }
+        update() {
+            super.update();
+            this.gravity+=0.1
+            this.y+=this.gravity
+        }
+        draw(context)
+        {
+            context.drawImage(this.image,this.x,this.y,this.size,this.size)
+        }
+    }
+    class Fire extends Particle{
+        constructor(game,x,y) {
+            super(game);
+            this.image=document.querySelector('#fire')
+            this.size=Math.random()*100+50
+            this.y=y
+            this.x=x
+            this.speedX=1
+            this.speedY=1
+            this.angle=0
+            this.va=Math.random()*0.5-0.2
 
+        }
+        update() {
+            super.update();
+            this.angle+=this.va
+            this.x+=Math.sin(this.angle*5)
+        }
+        draw(context)
+        {
+            context.save()
+            context.translate(this.x,this.y)
+            context.rotate(this.angle)
+            context.drawImage(this.image,-this.size*0.5,-this.size*0.5,this.size,this.size)
+            context.restore()
+        }
+    }
+    class CollisionAnimation {
+        constructor(game,x,y) {
+            this.game=game
+            this.image=document.querySelector('#boom')
+            this.swidth=100
+            this.sheight=90
+            this.sizeMod=Math.random()+0.5
+            this.width=this.swidth* this.sizeMod
+            this.height=this.sheight* this.sizeMod
+            this.x=x-this.width*0.5
+            this.y=y-this.height*0.5
+            this.frameX=0
+            this.maxFrame=4
+            this.markedForDeletion=false
+            this.fps=15
+            this.frameInterval=1000/this.fps
+            this.frameTimer=0
+        }
+        update(deltaTime)
+        {
+            this.x-=this.game.speed
+            if(this.frameTimer>this.frameInterval)
+            {
+                this.frameX++
+                this.frameTimer=0
+            }else this.frameTimer+=deltaTime
+            if(this.frameX>this.maxFrame) this.markedForDeletion=true
+
+        }
+        draw(context)
+        {
+            context.drawImage(this.image,this.frameX*this.swidth,0,this.sheight,this.sheight,this.x,this.y,this.width,this.height)
+        }
+    }
 
     //паралакс фон
     class Layer {
@@ -462,11 +632,19 @@ window.addEventListener('load',function (){
 
 
 //обновление сцены 60фпс
-    const game = new Game(canvas.width,canvas.height)
-    console.log(game)
+    let game = new Game(canvas.width,canvas.height)
     let lastTime =0
     function animate(timeStamp)
     {
+        document.querySelector('.btnLevel').addEventListener('click',()=>{
+            document.querySelector('#layer1').setAttribute('src',"img/level-2/layer-1.png")
+            document.querySelector('#layer2').setAttribute('src',"img/level-2/layer-2.png")
+            document.querySelector('#layer3').setAttribute('src',"img/level-2/layer-3.png")
+            document.querySelector('#layer4').setAttribute('src',"img/level-2/layer-4.png")
+            document.querySelector('#layer5').setAttribute('src',"img/level-2/layer-5.png")
+            game=new Game(canvas.width,canvas.height)
+        })
+
         const delTime = timeStamp - lastTime
         lastTime=timeStamp
         ctx.clearRect(0,0,canvas.width,canvas.height)
@@ -474,5 +652,5 @@ window.addEventListener('load',function (){
         game.draw(ctx)
         requestAnimationFrame(animate)
     }
-    animate(0)
+    animate(1)
 })
